f3ecca92be8c2e89bd441b8f492db57bd21ded8e
Souvik Roy <roysouvikr112@gmail.com>
2025-07-23 00:47:57 +0530

    Star Pattern

---
+++ AkshaySaini/StarPattern.js
@@ -0,0 +1,58 @@
+
+// Star Pattern : 
+ /*   
+    ****
+    ****
+    ****
+    ****   
+           */
+
+let n = 4;
+
+for(let i = 0; i < n; i ++){
+    let row = "";
+    for(let j = 0; j < n; j ++){
+        row = row + " * ";
+    }
+    console.log(row);
+}
+
+console.log("================================");
+
+ /*   
+    *
+    **
+    ***
+    ****   
+           */
+
+let s = 5;
+for(let k = 0; k < s; k ++){
+    let increment = "";
+    for(let h = 0; h <= k; h ++){
+        increment = increment + " * ";
+    }
+    console.log(increment);
+}
+
+console.log("================================");
+
+ /*   
+    1
+    1 2
+    1 2 3
+    1 2 3 4 
+    1 2 3 4 5
+                */
+
+let w = 5;
+for(let l = 0; l < w; l ++){
+    let increment = "";
+    for(let r = 0; r <= l; r ++){
+        increment = increment + (r + 1)
+    }
+    console.log(increment);
+}
+
+
+console.log("================================");
