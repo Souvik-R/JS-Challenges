0cbfee5dc46eac24ce868f32bec74cbf8d2a6edf
Souvik-R <souvikr212@gmail.com>
2025-07-26 22:27:24 +0530

    start pattern more

--- AkshaySaini/StarPattern.js
+++ AkshaySaini/StarPattern.js
@@ -49,14 +49,13 @@ let w = 5;
 for(let l = 0; l < w; l ++){
     let increment = "";
     for(let r = 0; r <= l; r ++){
-        increment = increment + (r + 1)
+        increment = increment + (r + 1) + " "
     }
     console.log(increment);
 }
 
 
 console.log("================================");
-
 
  /*   
     1
@@ -72,6 +71,25 @@ for(let o = 1; o <= num; o ++){
     let data = "";
     for(let p = 1; p <= o; p ++){
         data = data + (o) + " ";
+    }
+    console.log(data);
+}
+
+console.log("================================");
+
+ /*   
+    1 2 3 4 5
+    1 2 3 4 
+    1 2 3
+    1 2
+    1
+         */
+
+let m = 5;
+for (w = 0; w < m; w++) {
+    let data = "";
+    for (let r = 0; r < m-w; r++) {
+        data = data + (r + 1) + " ";
     }
     console.log(data);
 }
