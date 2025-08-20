056afc3d8cf5eaa79360de94c4415db7ecaa3888
Souvik-R <souvikr212@gmail.com>
2025-08-20 23:51:48 +0530

    changes

--- revision.js
+++ revision.js
@@ -1,16 +1,27 @@
 
-function findNegativeCount(array){
-    let count = 0;
-    for(i = 0; i < array.length; i ++){
-        if(array[i] < 0){
-            count = count + 1;
-        }
+// function findNegativeCount(array){
+//     let count = 0;
+//     for(i = 0; i < array.length; i ++){
+//         if(array[i] < 0){
+//             count = count + 1;
+//         }
+//     }
+//     return count;
+// }
+
+
+// const arr = [100, -40, -8, 6, -9];
+
+// const result = findNegativeCount(arr);
+// console.log(result);
+
+const obj = {
+    a: 1,
+    b: 2,
+    sum(){
+        return this.a + this.b
     }
-    return count;
-}
+};
+console.log(obj.sum());
 
 
-const arr = [100, -40, -8, 6, -9];
-
-const result = findNegativeCount(arr);
-console.log(result);
