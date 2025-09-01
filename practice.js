32181045358654a5e615b1ad97f1b4c93b0eb0fd
Souvik-R <souvikr212@gmail.com>
2025-09-02 01:22:13 +0530

    palindrome practice

--- practice.js
+++ practice.js
@@ -1,1 +1,16 @@
 // palindrome
+
+const palindrome = function(num){
+    let numCopy = num;
+    let reverse = 0;
+
+    while(num > 0){
+        let reminder = num % 10;
+        reverse = (10 * reverse) + reminder;
+        num = Math.floor(num/10);
+    }
+    return reverse == numCopy;
+}
+
+const results = palindrome(121);
+console.log(results);
