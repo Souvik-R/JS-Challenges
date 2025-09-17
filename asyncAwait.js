4d85b2dde980f6c7022276c545cd73bbe33f2000
Souvik-R <souvikr212@gmail.com>
2025-09-17 16:01:54 +0530

    async-await

---
+++ asyncAwait.js
@@ -0,0 +1,21 @@
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise resolve succesfully");
+    }, 10000);
+});
+
+async function handlePromise(){
+    console.log("HEYYYYYYYYYYYYY")
+    const val = await p;
+    console.log(val);
+    console.log("Namaste Javascript");
+}
+
+handlePromise();
+
+// function getData(){
+//     p.then((res) => console.log(res));
+//     console.log("Hello");
+// };
+// getData();
