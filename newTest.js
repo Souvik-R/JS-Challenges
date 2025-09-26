52b0f0c0c44009b4263f2365cfe709262d6e5926
Souvik-R <souvikr212@gmail.com>
2025-09-26 18:35:56 +0530

    changes

---
+++ newTest.js
@@ -0,0 +1,11 @@
+const input = [1, 3, 6, 10];
+const data = input.slice(1);
+console.log(data);
+
+const output = input.slice(1).map((num, i) => {
+  console.log(num, ":", i);
+  const diff = num - input[i];
+  return diff * diff;
+});
+
+console.log(output); // [4, 9, 16]
