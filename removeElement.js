b52f6594b4395fbeee8cd78f618ea892c2bab2f2
Souvik-R <souvikr212@gmail.com>
2025-11-20 23:54:37 +0530

    changes

--- removeElement.js
+++ removeElement.js
@@ -3,7 +3,6 @@ const arr = [1, 3, 3, 2, 5, 6, 7];
 function elementRemove(num, value){
     let x = 0;
     for(let i = 0; i < num.length; i ++){
-        // console.log(num[i], value);
         if(num[i] !== value){
             num[x] = num[i]
             x ++;
