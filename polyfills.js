f0940af56bdf346adc423916df34108d1c2ed5b3
Souvik-R <souvikr212@gmail.com>
2025-12-02 11:37:48 +0530

    polyfill of map

---
+++ polyfills.js
@@ -0,0 +1,17 @@
+
+
+const arr = [2, 3, 5, 7, 9];
+
+Array.prototype.myMap = function (callback){
+    let results = [];
+    this.forEach((item, index, array)=>{
+        results.push(callback(item, index, array));
+    });
+    return results;
+};
+
+const dataArray = arr.myMap((item) => {
+    return item * 2;
+});
+
+console.log(dataArray);
