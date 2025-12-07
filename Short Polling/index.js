2a7fcdcf1e5684399cc41a7b06e42f587b2eca58
Souvik-R <souvikr212@gmail.com>
2025-12-07 22:08:01 +0530

    changes

---
+++ Short Polling/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+let data = "Initial Data";
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.get("/getData", (req, res) => {
+    res.send({
+        data
+    });
+});
+
+app.get("/updateData", (req, res) => {
+    data = "Updated Data"
+    res.send({
+        data
+    });
+});
+
+
+const port = 5011;
+
+app.listen(port, () => {
+   console.log(`app is listening on port ${port}`);
+});
