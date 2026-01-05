f1a5f3cda5f7e2f8a08884479f1780a6d1a93872
Souvik-R <souvikr212@gmail.com>
2026-01-05 23:57:54 +0530

    changes

---
+++ AkshaySaini/Arrays-Easy-Medium/buyAndSellProduct.js
@@ -0,0 +1,21 @@
+// Best way to buy and sell a product.
+
+let maxProfitCalculation = function (prices) {
+    let min = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] - min > maxProfit) {
+            maxProfit = prices[i] - min;
+        }
+        if (prices[i] < min) {
+            min = prices[i];
+        }
+    }
+    return maxProfit;
+}
+
+const arr = [1, 3, 6, 2, 7, 5];
+const result = maxProfitCalculation(arr);
+
+console.log(result);
