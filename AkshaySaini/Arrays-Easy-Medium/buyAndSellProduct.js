3e6c8f485ec5d5bae6f9fe9de27b27e34b3b9779
Souvik-R <souvikr212@gmail.com>
2026-01-10 19:14:55 +0530

    changes

--- AkshaySaini/Arrays-Easy-Medium/buyAndSellProduct.js
+++ AkshaySaini/Arrays-Easy-Medium/buyAndSellProduct.js
@@ -2,7 +2,6 @@
 let maxProfitCalculation = function (prices) {
     let min = prices[0];
     let maxProfit = 0;
-
     for (let i = 0; i < prices.length; i++) {
         if (prices[i] - min > maxProfit) {
             maxProfit = prices[i] - min;
