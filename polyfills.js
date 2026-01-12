43ec7dbe535f21caebda4dfa17acf246f001dcd4
Souvik-R <souvikr212@gmail.com>
2026-01-12 15:52:34 +0530

    polyfill of Promise.all

--- polyfills.js
+++ polyfills.js
@@ -1,4 +1,4 @@
-
+// Polyfills of MAP function
 
 const arr = [2, 3, 5, 7, 9];
 
@@ -14,4 +14,32 @@ const dataArray = arr.myMap((item) => {
     return item * 2;
 });
 
-console.log(dataArray);
+// console.log(dataArray);
+
+// Polyfills of Promise.all function
+Promise.myPromiseAll = function (promises) {
+    return new Promise((resolve, reject) => {
+        if (!Array.isArray(promises)) {
+            return reject(new TypeError("Argument must be an array"));
+        };
+
+        const results = [];
+        let completed = 0;
+
+        if (promises.length == 0) {
+            return resolve([]);
+        };
+
+        promises.forEach((promise, i) => {
+            Promise.resolve(promise).then((value) => {
+                results[i] = value;
+                completed++;
+
+                if (completed == results[i]) {
+                    resolve(results);
+                };
+            }).catch(err => reject(err));
+        });
+    })
+}
+
