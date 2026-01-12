39d336bb47b0587ecfec35d767e76df78977a2aa
Souvik-R <souvikr212@gmail.com>
2026-01-13 00:15:36 +0530

    changes

--- polyfills.js
+++ polyfills.js
@@ -43,3 +43,20 @@ Promise.myPromiseAll = function (promises) {
     })
 }
 
+// Polyfills of Momoize function
+function memoize(fn){
+    const cache = {};
+
+    return function(args){
+
+        if(cache[args]){
+            return cache[args];
+        };
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+
+
