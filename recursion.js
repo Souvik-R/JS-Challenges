d75f453c9ebe3acbd0024f91e3db3c78d7456a2a
Souvik-R <souvikr212@gmail.com>
2026-02-21 00:28:10 +0530

    Print n to 1 using recursion

--- recursion.js
+++ recursion.js
@@ -2,10 +2,20 @@
 // Print 1 to n using recursion
 
 function print(x){
-    let num = 10;
-    if(x > num) return;
+    let num = 5;
+    if(x > num) return; // Base case
     console.log(x);
-    print(++x);
+    print(++x); // Recursive case
 }
 
 print(1);
+
+
+// Print n to 1 using recursion
+
+function fun(num){
+    if(num < 1) return; // Base case
+    console.log(num);
+    fun(--num); // Recursive case
+}
+fun(5)
