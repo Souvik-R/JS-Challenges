41806fe2e276340dcb9554ea699cd8aff316b821
Souvik-R <souvikr212@gmail.com>
2026-02-27 01:15:18 +0530

    changes

--- recursion.js
+++ recursion.js
@@ -1,21 +1,37 @@
 
-// Print 1 to n using recursion
+// // Print 1 to n using recursion
 
-function print(x){
-    let num = 5;
-    if(x > num) return; // Base case
-    console.log(x);
-    print(++x); // Recursive case
-}
+// function print(x){
+//     let num = 5;
+//     if(x > num) return; // Base case
+//     console.log(x);
+//     print(++x); // Recursive case
+// }
 
-print(1);
+// print(1);
 
 
-// Print n to 1 using recursion
+// // Print n to 1 using recursion
 
-function fun(num){
-    if(num < 1) return; // Base case
-    console.log(num);
-    fun(--num); // Recursive case
-}
-fun(5)
+// function fun(num){
+//     if(num < 1) return; // Base case
+//     console.log(num);
+//     fun(--num); // Recursive case
+// }
+// fun(5)
+
+
+let missingNumber = function(nums) {
+    let n = nums.length;
+    let totalSum = n * (n + 1) / 2;
+    let partialSum = 0
+    for(let i = 0; i < n; i++) {
+        partialSum = partialSum + nums[i];
+    }
+    return totalSum - partialSum;
+};
+
+let nums = [3, 0, 1];
+
+console.log("Array:", nums);
+console.log("Missing Number:", missingNumber(nums));
