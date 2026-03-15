f0b5a2e9156e382074bac1eca8aa479c312841ca
Souvik-R <souvikr212@gmail.com>
2026-03-15 12:33:10 +0530

    bubble sort

--- AkshaySaini/searching&sorting/bubbleSort.js
+++ AkshaySaini/searching&sorting/bubbleSort.js
@@ -1,1 +1,23 @@
 // 
+
+
+function bubbleSort(arr){
+    let n = arr.length;
+
+    for(let i = 0; i < n - 1; i++){
+        let isSwapped = false;
+        for(let j = 0; j < n - 1 - i; j++){
+            if(arr[j] > arr[j + 1]){
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSwapped = true;
+            }
+        }
+        if(!isSwapped) break;
+    }
+    return arr;
+}
+
+let array = [2, 6, 1, 8, 3, 0];
+console.log(bubbleSort(array));
