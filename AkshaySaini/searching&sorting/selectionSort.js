0f653e8802ed5fcec948357e0f4ed295c79c56db
Souvik-R <souvikr212@gmail.com>
2026-03-16 02:59:31 +0530

    selection & insertion sort

---
+++ AkshaySaini/searching&sorting/selectionSort.js
@@ -0,0 +1,19 @@
+const arr = [7, 8, 2, -1, 0, 9, 3];
+
+function selectionSort(arr){
+    let n = arr.length;
+    for(let i = 0; i < n - 1; i++){
+        let min = i;
+        for(let j = i + 1; j < n; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+
+console.log(selectionSort(arr));
