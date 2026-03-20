ec7be1756231a3c4b6a219857362abe3a9cd5f20
Souvik-R <souvikr212@gmail.com>
2026-03-20 19:03:58 +0530

    Merge Sort

---
+++ AkshaySaini/searching&sorting/mergeSort.js
@@ -0,0 +1,31 @@
+
+// Merge Sort : 
+
+let sortArray = function (nums) {
+    if (nums.length <= 1) return nums;
+    let mid = Math.floor(nums.length / 2);
+    let left = sortArray(nums.slice(0, mid));
+    let right = sortArray(nums.slice(mid));
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let result = [];
+    let i = 0, j = 0;
+    while (i < left.length && j < right.length) {
+        if(left[i] < right[j]){
+            result.push(left[i]);
+            i++
+        } else {
+            result.push(right[j]);
+            j++
+        }
+    }
+    return [...result, ...left.slice(i), ...right.slice(j)];
+}
+
+let array = [5, 2, 3, 1];
+
+let sortedArray = sortArray(array);
+
+console.log("Sorted Array:", sortedArray);
