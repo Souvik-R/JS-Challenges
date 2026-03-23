823b36c2a7667ded98006e146d17bb0ebe798daf
Souvik-R <souvikr212@gmail.com>
2026-03-23 23:59:06 +0530

    linked list - add node at head

---
+++ AkshaySaini/linkedList-Easy-Medium/addingNodes.js
@@ -0,0 +1,9 @@
+// add node to the head 
+
+// suppose i have a linked list and i want to add one more node to the head of the list
+
+function addAtHead(val){
+    let newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+}
