3e22baa11c06d6b7a9c91de5ac1005ca1060d32f
Souvik-R <souvikr212@gmail.com>
2026-03-24 23:50:52 +0530

    chnages

--- AkshaySaini/linkedList-Easy-Medium/addingNodes.js
+++ AkshaySaini/linkedList-Easy-Medium/addingNodes.js
@@ -2,6 +2,7 @@
 
 // suppose i have a linked list and i want to add one more node to the head of the list
 
+
 function addAtHead(val){
     let newNode = new Node(val);
     newNode.next = this.head;
