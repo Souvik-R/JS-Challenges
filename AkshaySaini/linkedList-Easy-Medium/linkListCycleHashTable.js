90732b3b7db3b34d645e09ecae582e663d2c0989
Souvik-R <souvikr212@gmail.com>
2026-04-01 00:31:55 +0530

    link list cycle or not - approach hash map

---
+++ AkshaySaini/linkedList-Easy-Medium/linkListCycleHashTable.js
@@ -0,0 +1,15 @@
+// find a linked list is a cycle or not 
+// Approach - Hash Table
+
+let hasCycle = function(head){
+    let seenNodes = new Set();
+    let curr = head;
+    while(curr){
+        if(seenNodes.has(curr)){
+            return true;
+        };
+        seenNodes.add(curr);
+        curr = curr.next;
+    };
+    return false;
+};
