4999b79abf93a426af4b4fcac7c413c202ec9975
Souvik-R <souvikr212@gmail.com>
2026-04-05 22:16:58 +0530

    changes

--- AkshaySaini/linkedList-Easy-Medium/linkListCycleHashTable.js
+++ AkshaySaini/linkedList-Easy-Medium/linkListCycleHashTable.js
@@ -1,11 +1,11 @@
 // find a linked list is a cycle or not 
 // Approach - Hash Table
 
-let hasCycle = function(head){
+let hasCycle = function (head) {
     let seenNodes = new Set();
     let curr = head;
-    while(curr){
-        if(seenNodes.has(curr)){
+    while (curr) {
+        if (seenNodes.has(curr)) {
             return true;
         };
         seenNodes.add(curr);
