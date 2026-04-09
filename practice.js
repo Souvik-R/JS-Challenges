fe712dceb6e6af1790b23bec417df15281a435aa
Souvik-R <souvikr212@gmail.com>
2026-04-09 23:24:38 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,16 +1,22 @@
-// palindrome
-const palindrome = function(num){
-    let numCopy = num;
-    let reverse = 0;
-
-    while(num > 0){
-        let reminder = num % 10;
-        reverse = (10 * reverse) + reminder;
-        num = Math.floor(num/10);
-    }
-    return reverse == numCopy;
-}
 
 
-const results = palindrome(121);
-console.log(results);
+// 1. merge two array
+
+const array1 = [2, 3, 8, 4];
+const array2 = [6, 9, 5, 1];
+
+let mergeArray = (arr1, arr2) => {
+    let result = [];
+
+    for(let i = 0; i < arr1.length; i++){
+        result.push(arr1[i]);
+    };
+
+    for(let j = 0; j < arr2.length; j++){
+        result.push(arr2[j]);
+    };
+
+    return result;
+};
+
+console.log(mergeArray(array1, array2));
