46074aba8fd64635a418b7f5d326375eefc925e7
Souvik-R <souvikr212@gmail.com>
2026-04-23 14:44:47 +0530

    odd even linked list

---
+++ AkshaySaini/linkedList-Easy-Medium/oddEvenLinkedList.js
@@ -0,0 +1,15 @@
+
+
+const oddEvenList = function(head){
+    let odd = head;
+    let even = head.next;
+    let evenStart = even;
+    while(odd.next && even.next){
+        odd.next = odd.next.next;
+        even.next = even.next.next;
+        odd = odd.next;
+        even = even.next;
+    }
+    odd.next = evenStart;
+    return head;
+}
