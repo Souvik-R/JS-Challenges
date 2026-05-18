78f6fa2fcaf160eb7c09497657916da35ebcc751
Souvik-R <souvikr212@gmail.com>
2026-05-18 23:49:12 +0530

    practice

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,20 +1,60 @@
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise resolve succesfully");
-  }, 10000);
-});
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise resolve succesfully");
+//   }, 10000);
+// });
 
-async function handlePromise() {
-  console.log("HEYYYYYYYYYYYYY");
-  const val = await p;
-  console.log(val);
-  console.log("Namaste Javascript");
+// async function handlePromise() {
+//   console.log("HEYYYYYYYYYYYYY");
+//   const val = await p;
+//   console.log(val);
+//   console.log("Namaste Javascript");
+// }
+
+// handlePromise();
+
+// // function getData(){
+// //     p.then((res) => console.log(res));
+// //     console.log("Hello");
+// // };
+// // getData();
+
+// debouncing
+
+// let count = 0;
+// const getData = function(){
+//   console.log("Called...", count++);
+// }
+
+
+// const betterFunc = function(fn){
+//   let timer;
+//   return function(){
+//     let context = this, args = arguments
+//     clearTimeout(timer);
+//     timer = setTimeout(()=>{
+//       fn.apply(context, args);
+//     }, 300)
+//   }
+// }
+
+
+// const doSomeMagic = betterFunc(getData)
+
+
+
+// Closure
+
+function outer(){
+  let count = 0;
+  return function inner(){
+    count++
+    return count;
+  }
 }
-
-handlePromise();
-
-// function getData(){
-//     p.then((res) => console.log(res));
-//     console.log("Hello");
-// };
-// getData();
+let data = outer();
+console.log(data());
+console.log(data());
+console.log(data());
+console.log(data());
+console.log(data());
