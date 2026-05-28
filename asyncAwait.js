773ca2c7b5cc761c83e68e22b964753c779501bb
Souvik-R <souvikr212@gmail.com>
2026-05-28 23:50:58 +0530

    change

--- asyncAwait.js
+++ asyncAwait.js
@@ -45,16 +45,81 @@
 
 // Closure
 
-function outer(){
-  let count = 0;
-  return function inner(){
-    count++
-    return count;
+// function outer(){
+//   let count = 0;
+//   return function inner(){
+//     count++
+//     return count;
+//   }
+// }
+// let data = outer();
+// console.log(data());
+// console.log(data());
+// console.log(data());
+// console.log(data());
+// console.log(data());
+
+
+// const str = "I love javascript";
+
+// const count = str.split(" ").reduce((acc) => {
+//     return acc + 1;
+// }, 0);
+
+// console.log(count);
+
+// const str = "hello world";
+
+// const result = str
+//     .split(" ")
+//     .map(word => word[0].toUpperCase() + word.slice(1))
+//     .join(" ");
+
+// console.log(result);
+
+
+
+
+
+// const arr = [1, [2, 3, [4, 5, [6]]], 7];
+
+// function flattenArray(result) {
+//     console.log(result, "1111111111111111");
+//     // console.log(result, "1111111111111111");
+//     let swapped = true;
+//     while (swapped) {
+//         swapped = false;
+//         for (let i = 0; i < result.length; i++) {
+//             if (Array.isArray(result[i])) {
+//                 // remove nested array
+//                 let temp = result[i];
+//                 result.splice(i, 1, ...temp);
+//                 swapped = true;
+//             }
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(flattenArray(arr));
+
+
+
+function moveOnesOptimized(arr) {
+  let left = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      // swap
+      let temp = arr[left];
+      arr[left] = arr[i];
+      arr[i] = temp;
+
+      left++;
+    }
   }
+
+  return arr;
 }
-let data = outer();
-console.log(data());
-console.log(data());
-console.log(data());
-console.log(data());
-console.log(data());
+
+console.log(moveOnesOptimized([1, 0, 1, 0, 1]));
