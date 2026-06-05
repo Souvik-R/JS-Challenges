0a828a1126d9f83d141e2028230bfe7a0bf6a635
Souvik-R <souvikr212@gmail.com>
2026-06-05 15:08:06 +0530

    find words coantaining character

---
+++ AkshaySaini/strings/findWordContainingCharacter.js
@@ -0,0 +1,16 @@
+
+
+
+
+var findWordsContaining = function(words, x) {
+    let res = [];
+    for(let i = 0; i < words.length; i++){
+        for(j = 0; j < words[i].length; j++){
+            if(words[i][j] === x){
+                res.push(i);
+                break;
+            }
+        }
+    }
+    return res;
+};
