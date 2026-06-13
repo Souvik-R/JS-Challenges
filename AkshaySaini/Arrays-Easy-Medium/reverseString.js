e3e466f0c171407a2b0c74b203f79550645c88d6
Souvik-R <souvikr212@gmail.com>
2026-06-13 23:30:52 +0530

    changes

--- AkshaySaini/Arrays-Easy-Medium/reverseString.js
+++ AkshaySaini/Arrays-Easy-Medium/reverseString.js
@@ -3,7 +3,6 @@
 // Write a function that reverses a string. The input string is given as an array of characters s.
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
-
 const reverseString = function(s){
     const arr = s.split("");
     const len = arr.length;
